44be8fff73b8865680db80ae0c81013c4684f3f8
Luz Hernandez <hernandezluz18@gmail.com>
2024-02-03 08:35:49 -0500

    adding build folder

---
+++ frontend/build/precache-manifest.5828730fe3860e667817b85bda6423ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceeed5f3b2b516b0659ca7f725c6587b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e96d9b8d46fcb0b4c46",
+    "url": "/static/css/2.016185da.chunk.css"
+  },
+  {
+    "revision": "2ad517814704954ae9bf",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1e96d9b8d46fcb0b4c46",
+    "url": "/static/js/2.a2d20c93.chunk.js"
+  },
+  {
+    "revision": "2ad517814704954ae9bf",
+    "url": "/static/js/main.e3ee1a4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
